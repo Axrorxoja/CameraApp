ab3390c0a81a93038c1e1b10844f6296737fbf77
Akhrorkhoja <akhrorkhoja@mysuperdispatch.com>
2019-11-20 01:13:56 +0500

    Added video capture fragment

--- app/src/main/java/com/example/cameraapp/fragments/ImageCaptureFragment.kt
+++ app/src/main/java/com/example/cameraapp/fragments/ImageCaptureFragment.kt
@@ -6,13 +6,11 @@ import AutoFitPreviewBuilder
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import android.location.Location
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.camera.core.*
 import androidx.camera.core.ImageCapture.CaptureMode
-import androidx.camera.core.ImageCapture.Metadata
 import androidx.camera.view.TextureViewMeteringPointFactory
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.setPadding
@@ -36,9 +34,8 @@ import java.util.concurrent.Executors
  */
 private const val FILENAME = "yyyy-MM-dd-HH-mm-ss-SSS"
 private const val PHOTO_EXTENSION = ".jpg"
-private const val VIDEO_EXTENSION = ".mp4"
 
-class CameraFragment : Fragment(R.layout.fragment_camera) {
+class ImageCaptureFragment : Fragment(R.layout.fragment_camera) {
 
     private lateinit var outputDirectory: File
     private val executor by lazy(LazyThreadSafetyMode.NONE) { Executors.newSingleThreadExecutor() }
@@ -46,7 +43,6 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
     private var lensFacing = CameraX.LensFacing.BACK
     private var preview: Preview? = null
     private var imageCapture: ImageCapture? = null
-    private var videoCapture: VideoCapture?=null
 
 
     override fun onResume() {
@@ -55,7 +51,7 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
         //  while the app was on paused state
         if (!PermissionsFragment.hasPermissions(requireContext())) {
             Navigation.findNavController(requireActivity(), R.id.fragment_container).navigate(
-                CameraFragmentDirections.actionCameraToPermissions()
+                ImageCaptureFragmentDirections.actionCameraToPermissions()
             )
 
         }
@@ -74,18 +70,6 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
         }
     }
 
-    private val videoCaptureLister = object:VideoCapture.OnVideoSavedListener{
-        override fun onError(
-            videoCaptureError: VideoCapture.VideoCaptureError,
-            message: String,
-            cause: Throwable?
-        ) {
-        }
-        override fun onVideoSaved(file: File) {
-
-        }
-    }
-
     @SuppressLint("MissingPermission", "ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -100,7 +84,7 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
             bindCameraUseCases()
 
         }
-        view_finder.setOnTouchListener { v, motionEvent ->
+        view_finder.setOnTouchListener { _, motionEvent ->
             val point = TextureViewMeteringPointFactory(view_finder)
                 .createPoint(motionEvent.x, motionEvent.y)
             val action = FocusMeteringAction.Builder
@@ -127,25 +111,12 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
         // Use the auto-fit preview builder to automatically handle size and orientation changes
         preview = AutoFitPreviewBuilder.build(viewFinderConfig, view_finder)
 
-       setUpImageCapture()
-
-       setUpVideoCapture()
+        setUpImageCapture()
 
         // Apply declared configs to CameraX using the same lifecycle owner
         CameraX.bindToLifecycle(
-            viewLifecycleOwner, preview, videoCapture
+            viewLifecycleOwner, preview, imageCapture
         )
-    }
-
-    private fun setUpVideoCapture() {
-        val videoCaptureConfig = VideoCaptureConfig.Builder().apply {
-            setLensFacing(lensFacing)
-            setTargetAspectRatio(AspectRatio.RATIO_4_3)
-            setTargetRotation(view_finder.display.rotation)
-
-        }.build()
-
-        videoCapture = VideoCapture(videoCaptureConfig)
     }
 
     private fun setUpImageCapture() {
@@ -178,41 +149,33 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
             View.inflate(requireContext(), R.layout.camera_ui_container, camera_container)
 
         // Listener for button used to capture photo
-        camera_capture_button.setOnLongClickListener {
-            videoCapture?.apply {
-                stopRecording()
-            }
-            true
-        }
         camera_capture_button.setOnClickListener {
             // Get a stable reference of the modifiable image capture use case
-            videoCapture?.apply {
+            imageCapture?.apply {
 
                 // Create output file to hold the image
                 val photoFile = createFile(outputDirectory)
 
 //                // Setup image capture metadata
-//                val metadata = Metadata().apply {
-//                    // Mirror image when using the front camera
-//                    isReversedHorizontal = lensFacing == CameraX.LensFacing.FRONT
-//                }
-                val metadata = VideoCapture.Metadata().apply {
+                val metadata = ImageCapture.Metadata().apply {
+                    // Mirror image when using the front camera
+                    isReversedHorizontal = lensFacing == CameraX.LensFacing.FRONT
                 }
 
                 // Setup image capture listener which is triggered after photo has been taken
-                startRecording(photoFile, executor, videoCaptureLister)
+                takePicture(photoFile, metadata, executor, imageSavedListener)
 
-                // We can only change the foreground Drawable using API level 23+ API
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//
-//                    // Display flash animation to indicate that photo was captured
-//                    camera_container.postDelayed({
-//                        camera_container.foreground = ColorDrawable(Color.WHITE)
-//                        camera_container.postDelayed(
-//                            { camera_container.foreground = null }, ANIMATION_FAST_MILLIS
-//                        )
-//                    }, ANIMATION_SLOW_MILLIS)
-//                }
+//                 We can only change the foreground Drawable using API level 23+ API
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                    // Display flash animation to indicate that photo was captured
+                    camera_container.postDelayed({
+                        camera_container.foreground = ColorDrawable(Color.WHITE)
+                        camera_container.postDelayed(
+                            { camera_container.foreground = null }, ANIMATION_FAST_MILLIS
+                        )
+                    }, ANIMATION_SLOW_MILLIS)
+                }
             }
         }
 
@@ -238,7 +201,7 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
         // Listener for button used to view last photo
         photo_view_button.setOnClickListener {
             Navigation.findNavController(requireActivity(), R.id.fragment_container).navigate(
-                CameraFragmentDirections.actionCameraToGallery(outputDirectory.absolutePath)
+                ImageCaptureFragmentDirections.actionCameraToGallery(outputDirectory.absolutePath)
             )
         }
     }
---
+++ app/src/main/java/com/example/cameraapp/fragments/VideoCaptureFragment.kt
@@ -0,0 +1,194 @@
+package com.example.cameraapp.fragments
+
+import AutoFitPreviewBuilder
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import androidx.camera.core.*
+import androidx.camera.view.TextureViewMeteringPointFactory
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.setPadding
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.cameraapp.MainActivity
+import com.example.cameraapp.R
+import kotlinx.android.synthetic.main.camera_ui_container.*
+import kotlinx.android.synthetic.main.fragment_camera.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.Executors
+
+/**
+ * Main fragment for this app. Implements all camera operations including:
+ * - Viewfinder
+ * - Video taking
+ */
+private const val FILENAME = "yyyy-MM-dd-HH-mm-ss-SSS"
+private const val VIDEO_EXTENSION = ".mp4"
+
+class VideoCaptureFragment : Fragment(R.layout.fragment_camera) {
+
+    private lateinit var outputDirectory: File
+    private val executor by lazy(LazyThreadSafetyMode.NONE) { Executors.newSingleThreadExecutor() }
+
+    private var lensFacing = CameraX.LensFacing.BACK
+    private var preview: Preview? = null
+    private var imageCapture: ImageCapture? = null
+    private var videoCapture: VideoCapture? = null
+
+
+    override fun onResume() {
+        super.onResume()
+        // Make sure that all permissions are still present, since user could have removed them
+        //  while the app was on paused state
+        if (!PermissionsFragment.hasPermissions(requireContext())) {
+//            Navigation.findNavController(requireActivity(), R.id.fragment_container).navigate(
+//                VideoCa.actionCameraToPermissions()
+//            )
+
+        }
+    }
+
+    private val videoCaptureLister = object : VideoCapture.OnVideoSavedListener {
+        override fun onError(
+            videoCaptureError: VideoCapture.VideoCaptureError,
+            message: String,
+            cause: Throwable?
+        ) {
+        }
+
+        override fun onVideoSaved(file: File) {
+
+        }
+    }
+
+    @SuppressLint("MissingPermission", "ClickableViewAccessibility")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Determine the output directory
+        outputDirectory = MainActivity.getOutputDirectory(requireContext())
+
+        // Wait for the views to be properly laid out
+        view_finder.post {
+
+            // Build UI controls and bind all camera use cases
+            updateCameraUi()
+            bindCameraUseCases()
+
+        }
+        view_finder.setOnTouchListener { v, motionEvent ->
+            val point = TextureViewMeteringPointFactory(view_finder)
+                .createPoint(motionEvent.x, motionEvent.y)
+            val action = FocusMeteringAction.Builder
+                .from(point)
+                .build()
+            CameraX.getCameraControl(CameraX.LensFacing.BACK).startFocusAndMetering(action)
+            false
+        }
+    }
+
+    /** Declare and bind preview, capture and analysis use cases */
+    private fun bindCameraUseCases() {
+
+        // Set up the view finder use case to display camera preview
+        val viewFinderConfig = PreviewConfig.Builder().apply {
+            setLensFacing(lensFacing)
+            // We request aspect ratio but no resolution to let CameraX optimize our use cases
+            setTargetAspectRatio(AspectRatio.RATIO_4_3)
+            // Set initial target rotation, we will have to call this again if rotation changes
+            // during the lifecycle of this use case
+            setTargetRotation(view_finder.display.rotation)
+        }.build()
+
+        // Use the auto-fit preview builder to automatically handle size and orientation changes
+        preview = AutoFitPreviewBuilder.build(viewFinderConfig, view_finder)
+
+        setUpVideoCapture()
+
+        // Apply declared configs to CameraX using the same lifecycle owner
+        CameraX.bindToLifecycle(
+            viewLifecycleOwner, preview, imageCapture
+        )
+    }
+
+    private fun setUpVideoCapture() {
+        val videoCaptureConfig = VideoCaptureConfig.Builder().apply {
+            setLensFacing(lensFacing)
+            setTargetAspectRatio(AspectRatio.RATIO_4_3)
+            setTargetRotation(view_finder.display.rotation)
+
+        }.build()
+
+        videoCapture = VideoCapture(videoCaptureConfig)
+    }
+
+    /** Method used to re-draw the camera UI controls, called every time configuration changes */
+    @SuppressLint("RestrictedApi")
+    private fun updateCameraUi() {
+
+        // Remove previous UI if any
+        camera_container.findViewById<ConstraintLayout>(R.id.camera_ui_container)?.let {
+            camera_container.removeView(it)
+        }
+
+        // Inflate a new view containing all UI for controlling the camera
+        val controls =
+            View.inflate(requireContext(), R.layout.camera_ui_container_with_out_switch, camera_container)
+
+        // Listener for button used to capture photo
+        camera_capture_button.setOnLongClickListener {
+            videoCapture?.apply {
+                stopRecording()
+            }
+            true
+        }
+        camera_capture_button.setOnClickListener {
+            // Get a stable reference of the modifiable image capture use case
+            videoCapture?.apply {
+
+                // Create output file to hold the image
+                val photoFile = createFile(outputDirectory)
+
+                val metadata = VideoCapture.Metadata()
+                    .apply {
+                    }
+
+                // Setup image capture listener which is triggered after photo has been taken
+                startRecording(photoFile, metadata, executor, videoCaptureLister)
+
+            }
+        }
+
+        // Listener for button used to view last photo
+        photo_view_button.setOnClickListener {
+//            Navigation.findNavController(requireActivity(), R.id.fragment_container).navigate(
+//                CameraFragmentDirections.actionCameraToGallery(outputDirectory.absolutePath)
+//            )
+        }
+    }
+
+    private fun setGalleryThumbnail(file: File) {
+        // Run the operations in the view's thread
+        photo_view_button.post {
+
+            // Remove thumbnail padding
+            photo_view_button.setPadding(resources.getDimension(R.dimen.stroke_small).toInt())
+
+            // Load thumbnail into circular button using Glide
+            Glide.with(photo_view_button)
+                .load(file)
+                .apply(RequestOptions.circleCropTransform())
+                .into(photo_view_button)
+        }
+    }
+
+    /** Helper function used to create a timestamped file */
+    private fun createFile(baseFolder: File) =
+        File(
+            baseFolder, SimpleDateFormat(FILENAME, Locale.US)
+                .format(System.currentTimeMillis()) + VIDEO_EXTENSION
+        )
+}
